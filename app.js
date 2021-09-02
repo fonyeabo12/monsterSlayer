7945d33a5932de55a45a2d53906a2a7b7de00d0f
FrankBlack <fonyeabo12@gmail.com>
2021-09-02 13:22:31 -0500

    Dynamic decrease of strength

--- app.js
+++ app.js
@@ -2,22 +2,40 @@ function getRandomValue(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+let currentRound = 0;
+
 const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
   },
   methods: {
     attackMonster() {
-      const attackValue = getRandomValu(5, 12);
+      this.currentRound++;
+      const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     },
   },
 });
